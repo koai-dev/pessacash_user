70f93c806cc2180e77701fa55a1965a3bfc2712b
koai-dev <khanhkimhn17@gmail.com>
2023-03-25 16:49:08 +0700

    update dev

--- lib/view/screens/auth/login/login_screen.dart
+++ lib/view/screens/auth/login/login_screen.dart
@@ -1,4 +1,4 @@
-import 'package:country_code_picker/country_code.dart';
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
